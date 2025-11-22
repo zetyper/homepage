a23ce7b3760c2ad733427347fbcd195b887663c7
zetyper <jaku.typing@gmail.com>
2025-11-23 07:27:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from quart import Quart
+from quart import Quart,render_template
 
 app = Quart(__name__)
 
 @app.route("/")
 async def hello():
-    return "Hello, Quart!"
-
+    return render_template("index.html")
+    
 if __name__ == "__main__":
     app.run()
